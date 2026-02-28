d5024913dd0934575edb65bf4f08cfa9cda3f5cc
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2026-02-28 16:53:13 +0200

    player flash on damage + damage handling to just player script

--- Code/damage.gd
+++ Code/damage.gd
@@ -1,31 +1,9 @@
 extends Area2D
 
-var player_in_spike := false
-var cbody 
-
-
-func _process(_delta: float) -> void:
-	if player_in_spike:
-		if cbody.is_in_group("Player"):
-			if $"../../../Player".No_damage_time_active == false:
-				print("damage")
-				$"../../../Player".Health -=1
-				print($"../../../Player".Health)
-				$"../../../Player".No_damage_time.start()
-				$"../../../Player".No_damage_time_active = true
-				if $"../../../Player".Health == 0:
-					print("you died")
-					get_tree().reload_current_scene()
-
+# On the spike, emit a signal or just call the player directly
 func _on_body_entered(body: CharacterBody2D) -> void:
-	player_in_spike = true
-	cbody = body
-
-
-func _on_no_damage_time_timeout() -> void:
-	$"../../../Player".No_damage_time_active = false
-
+	if body.is_in_group("Player"):
+		body.take_damage()  # let the player handle its own damage
 
 func _on_body_exited(body: CharacterBody2D) -> void:
-	player_in_spike = false
-	cbody = body
+	pass  # probably don't need anything here
--- Code/player.gd
+++ Code/player.gd
@@ -9,11 +9,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var gravity_modifier: float = 1
 var water := true
 
-
-
 const SPEED := 200.0
 const JUMP_VELOCITY := -260.0
-
 
 func _physics_process(delta: float) -> void:
 	
@@ -49,27 +46,30 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-
-
-
+func take_damage() -> void:
+	if No_damage_time_active:
+		return
+	No_damage_time_active = true
+	Health -= 1
+	$AnimationPlayer.play("playeflash")
+	No_damage_time.start()
+	print(Health)
+	if Health <= 0:
+		get_tree().reload_current_scene()
 
 func _on_air_body_entered(body: CharacterBody2D) -> void:
 	if body.is_in_group("Player"):
 		water = false
 
 func _on_air_body_exited(body: CharacterBody2D) -> void:
-	
 	if body.is_in_group("Player"):
 		water = true
 
-
-func _on_damage_damage() -> void:
-	print("damage yes")
-	if No_damage_time_active == false:
-		Health -= 1
-		print(Health)
-		No_damage_time.start()
-
-
 func _on_no_damage_time_timeout() -> void:
 	No_damage_time_active = false
+	$AnimationPlayer.stop()
+	$Sprite2D.modulate.a = 1.0  # make sure player is fully visible again
+
+func _ready() -> void:
+	# default
+	$Sprite2D.modulate.a = 1.0
