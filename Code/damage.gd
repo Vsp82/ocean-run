8ea6695082f9dafb8ca8822cfbd442a79d4fe2de
Vsp82 <viljo.elias@gmail.com>
2026-02-28 21:50:34 +0200

    damage from squid

--- Code/damage.gd
+++ Code/damage.gd
@@ -8,7 +8,7 @@ func _on_body_entered(body: CharacterBody2D) -> void:
 		take_damage = true
 		while take_damage:
 			body.take_damage()  # let the player handle its own damage
-			if $"../../../Player".Health == 0:
+			if body.Health == 0:
 				return
 			await wait(0.5)
 
--- Code/squid.gd
+++ Code/squid.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
 
-const speed = 1000
+const  speed = 350
 const ACCEL = 2000
 
-var v_u := false
 var vision := false
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+var moove := 5
+var time := true
+
 
 func _physics_process(delta: float) -> void:
-	if vision:
-		var direction = to_local(nav_agent.get_next_path_position()).normalized()
+	var direction = to_local(nav_agent.get_next_path_position()).normalized()
+	if vision and time:
+		if direction.x < 0:
+			$Sprite2D.flip_h = true
+		elif direction.x > 0:
+			$Sprite2D.flip_h = false
 		velocity = direction * speed
-
+		#velocity.x = move_toward(velocity.x, direction.x * speed, ACCEL * delta)
+		#velocity.y = move_toward(velocity.y, direction.y * speed, ACCEL * delta)
 		move_and_slide()
-		velocity = direction * 0
-		vision = false
-		await wait(1)
-		if v_u:
-			vision = true
+		moove -= 1
+		if moove == 0:
+			moove = 5
+			time = false
+			$Timer2.start()
 
 func makepath() -> void:
 	nav_agent.target_position = player.global_position
@@ -30,13 +37,10 @@ func _on_timer_timeout() -> void:
 func _on_vision_body_entered(body: CharacterBody2D) -> void:
 	if body.is_in_group("Player"):
 		vision = true
-		v_u = true
-		
-func _on_vision_body_exited(body: CharacterBody2D) -> void:
-	if body.is_in_group("Player"):
-		vision = false
-		v_u = false
-		
+
 func wait(seconds: float):
 	await get_tree().create_timer(seconds).timeout
-	
+
+
+func _on_timer_2_timeout() -> void:
+	time = true
